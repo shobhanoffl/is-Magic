dc8537d666e11372f002cfcd5142096612a06788
shobhanoffl <shobhankumar.20ads@sonatech.ac.in>
2023-07-23 14:09:49 +0530

    first commit

--- src/Main.java
+++ src/Main.java
@@ -4,18 +4,15 @@ public class Main {
     public static int fdNo(int no){
         if(no>9){
             String strNo = String.valueOf(no);
-            int tens = 1;
-            int len = 0;
-            while(tens<=no){
-                tens+=10;
-                len+=1;
+            int len = strNo.length();
+            int[] array = new int[len];
+            for(int i=len-1; i>=0; i--){
+                array[i]=no%10;
+                no/=10;
             }
-            int[] digits = new int[len];
-
             int result=0;
-            for(int i=0;i<digits.length;i++){
-                result+=Integer.parseInt(String.valueOf(digits[0]));
-                System.out.println(result+" "+digits[i]);
+            for(int i=0;i<len;i++){
+                result+=array[i];
             }
             return fdNo(result);
         }else{
@@ -23,6 +20,6 @@ public class Main {
         }
     }
     public static void main(String[] args) {
-        System.out.println(fdNo(28));
+        System.out.println(fdNo(55));
     }
 }
